7fdc3869bb481f0bfc9600e982eb482eee1b751e
Miquel Jayson de Leeuw <jaysondlw@gmail.com>
2024-11-09 20:31:24 +0100

    Setting values of specific LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -4,91 +4,71 @@
 
 #define LED_FREQ 12000
 #define LED_RESOLUTION 8
+#define LED_BRIGHTNESS 200
 
 #define RED_LED_PIN 21
+#define RED_LED_CH 0
+
 #define GREEN_LED_PIN 22
+#define GREEN_LED_CH 1
+
 #define BLUE_LED_PIN 23
+#define BLUE_LED_CH 2
 
-uint8_t color = 0;         // a value from 0 to 255 representing the hue
-uint32_t R, G, B;          // the Red Green and Blue color components
-uint8_t brightness = 255;  // 255 is maximum brightness, but can be changed.  Might need 256 for common anode to fully turn off.
-
-void hueToRGB(uint8_t hue, uint8_t brightness) {
-  uint16_t scaledHue = (hue * 6);
-  uint8_t segment = scaledHue / 256;                     // segment 0 to 5 around the
-                                                         // color wheel
-  uint16_t segmentOffset = scaledHue - (segment * 256);  // position within the segment
-
-  uint8_t complement = 0;
-  uint16_t prev = (brightness * (255 - segmentOffset)) / 256;
-  uint16_t next = (brightness * segmentOffset) / 256;
-
-  brightness = 255 - brightness;
-  complement = 255;
-  prev = 255 - prev;
-  next = 255 - next;
-
-  switch (segment) {
-    case 0:  // red
-      R = brightness;
-      G = next;
-      B = complement;
-      break;
-    case 1:  // yellow
-      R = prev;
-      G = brightness;
-      B = complement;
-      break;
-    case 2:  // green
-      R = complement;
-      G = brightness;
-      B = next;
-      break;
-    case 3:  // cyan
-      R = complement;
-      G = prev;
-      B = brightness;
-      break;
-    case 4:  // blue
-      R = next;
-      G = complement;
-      B = brightness;
-      break;
-    case 5:  // magenta
-    default:
-      R = brightness;
-      G = complement;
-      B = prev;
-      break;
-  }
+void setRedValue(int brightness) {
+  ledcWrite(RED_LED_CH, brightness);
 }
 
+void setGreenValue(int brightness) {
+  ledcWrite(GREEN_LED_CH, brightness);
+}
 
+void setBlueValue(int brightness) {
+  ledcWrite(BLUE_LED_CH, brightness);
+
+}
+
+void clearLEDs() {
+  setRedValue(0);
+  setGreenValue(0);
+  setBlueValue(0);
+}
+
+void initLEDs() {
+  std::cout << "Loaded [initLEDs()]" << std::endl;
+  ledcAttachPin(RED_LED_PIN, RED_LED_CH);
+  ledcSetup(RED_LED_CH, LED_FREQ, LED_RESOLUTION);
+
+  ledcAttachPin(GREEN_LED_PIN, GREEN_LED_CH);
+  ledcSetup(GREEN_LED_CH, LED_FREQ, LED_RESOLUTION);
+
+  ledcAttachPin(BLUE_LED_PIN, BLUE_LED_CH);
+  ledcSetup(BLUE_LED_CH, LED_FREQ, LED_RESOLUTION);
+
+  clearLEDs();
+}
+
+void initBuzzer() {
+  std::cout << "Loaded [initBuzzer()]" << std::endl;
+}
 
 void setup() {
-  std::cout << "Setting up modules..." << std::endl;
-
-  ledcAttachPin(RED_LED_PIN, 0);
-  ledcSetup(0, LED_FREQ, LED_RESOLUTION);
-
-  ledcAttachPin(GREEN_LED_PIN, 1);
-  ledcSetup(1, LED_FREQ, LED_RESOLUTION);
-
-  ledcAttachPin(BLUE_LED_PIN, 2);
-  ledcSetup(2, LED_FREQ, LED_RESOLUTION);
+  initLEDs();
+  initBuzzer();
 }
 
 void loop() {
-  for (color = 0; color < 255; color++) {
+  clearLEDs();
+  setRedValue(LED_BRIGHTNESS);
+  delay(2500);
 
-    hueToRGB(color, brightness);
+  clearLEDs();
+  setGreenValue(LED_BRIGHTNESS);
+  delay(2500);
 
-    ledcWrite(0, R);
-    ledcWrite(1, G);
-    ledcWrite(2, B);
-
-    delay(100);
-  }
+  clearLEDs();
+  setBlueValue(LED_BRIGHTNESS);
+  delay(2500);
 
   std::cout << "Next cycle..." << std::endl;
 }
